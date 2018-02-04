499080040cdffaa2138d0ea8b0b403b14acd9367
realkewal <kew4l@yahoo.com.au>
2018-02-04 14:29:54 +1100

    Start with create-react-app

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+
+class App extends Component {
+    constructor () {
+        super();
+        this.state = {
+            robots: [],
+            searchfield: ''
+        };
+    };
+
+    async componentDidMount () {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const users = await response.json();
+        this.setState({ robots: users });
+    };
+ 
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value});
+    };
+    
+    render () {
+        const filteredRobots = this.state.robots.filter((robot) => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+        return(
+        <div className = 'tc' >
+                <h1>RoboSearch</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
+                    <CardList robots={filteredRobots} />
+                </Scroll>
+        </div>
+    );
+    };
+    
+};
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,76 @@
+export const robots = [{
+    "id": 1,
+    "name": "Burt Roser",
+    "username": "broser0",
+    "email": "broser0@live.com"
+}, {
+    "id": 2,
+    "name": "Jethro Birkinshaw",
+    "username": "jbirkinshaw1",
+    "email": "jbirkinshaw1@omniture.com"
+}, {
+    "id": 3,
+    "name": "Darrin Priestnall",
+    "username": "dpriestnall2",
+    "email": "dpriestnall2@geocities.jp"
+}, {
+    "id": 4,
+    "name": "Robinett Faro",
+    "username": "rfaro3",
+    "email": "rfaro3@myspace.com"
+}, {
+    "id": 5,
+    "name": "Thedrick Attawell",
+    "username": "tattawell4",
+    "email": "tattawell4@sogou.com"
+}, {
+    "id": 6,
+    "name": "Celinda Botterman",
+    "username": "cbotterman5",
+    "email": "cbotterman5@ucla.edu"
+}, {
+    "id": 7,
+    "name": "Christiano Doddemeade",
+    "username": "cdoddemeade6",
+    "email": "cdoddemeade6@tiny.cc"
+}, {
+    "id": 8,
+    "name": "Monah Meryett",
+    "username": "mmeryett7",
+    "email": "mmeryett7@craigslist.org"
+}, {
+    "id": 9,
+    "name": "Glyn Edgerley",
+    "username": "gedgerley8",
+    "email": "gedgerley8@reuters.com"
+}, {
+    "id": 10,
+    "name": "Blythe Gabites",
+    "username": "bgabites9",
+    "email": "bgabites9@ask.com"
+}, {
+    "id": 11,
+    "name": "Nessie Ruperti",
+    "username": "nrupertia",
+    "email": "nrupertia@dagondesign.com"
+}, {
+    "id": 12,
+    "name": "Debbi Gieves",
+    "username": "dgievesb",
+    "email": "dgievesb@godaddy.com"
+}, {
+    "id": 13,
+    "name": "Gannie Tolman",
+    "username": "gtolmanc",
+    "email": "gtolmanc@harvard.edu"
+}, {
+    "id": 14,
+    "name": "Westbrooke Ricco",
+    "username": "wriccod",
+    "email": "wriccod@ucla.edu"
+}, {
+    "id": 15,
+    "name": "Tera Sebire",
+    "username": "tsebiree",
+    "email": "tsebiree@slate.com"
+}]
